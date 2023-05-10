8ec8838d41d84b03351e74136c6c19fd534cfd23
JD Wang <43763092+jdw1023@users.noreply.github.com>
2023-05-10 16:15:10 -0400

    Update vite.config.ts
    temporary fix while github page is having issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
 	plugins: [],
 	server: { host: '0.0.0.0', port: 8000 },
 	clearScreen: false,
-	base:"/group-project-build-blocks/",
+	base:"/",
 	build: {
     rollupOptions: {
       input: {
